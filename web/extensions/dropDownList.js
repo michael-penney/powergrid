b8311e58f39ea5ccc5de6d890fcaf6ba81d0c89e
Anton Filippovich <anton.filippovich@pearlchain.net>
2019-05-28 14:19:23 +0700

    Did small refactoring

--- web/extensions/dropDownList.js
+++ web/extensions/dropDownList.js
@@ -18,19 +18,24 @@ define(['../override', 'jquery', '../utils'], function(override, $, utils) {
                 return {
                     renderCellContent: function(record, column, value) {
                         const isDropDownListCol = column.type === 'dropdown';
-                        if(!isDropDownListCol) {
+
+                        if (!isDropDownListCol) {
                             return $super.renderCellContent(record, column, value);
                         }
+
                         const cellContent = this.dropDownList.renderDropDownList(record, column, value);
                         const fragment = document.createDocumentFragment();
+
                         fragment.appendChild(cellContent);
                         fragment.appendChild($super.renderCellContent(record, column, value));
+
                         return fragment;
                     },
 
                     init: function() {
                         $super.init();
                         this.container.on('change', '.pg-dropDownList', function() {
+
                             let cell = $(this).parents('.pg-cell:eq(0)'),
                                 row = cell.parents('.pg-row:eq(0)'),
                                 key = cell.attr('data-column-key'),
@@ -38,12 +43,14 @@ define(['../override', 'jquery', '../utils'], function(override, $, utils) {
 
                             let listId = rowId + '-key-' + key;
                             let val = $('#' + listId + ' option:selected').text();
+
                             grid.dataSource.setValue(rowId, key, val);
                         });
                     },
 
                     dropDownList: {
                         renderDropDownList: function(record, column) {
+
                             let cellContent = document.createElement('div');
                             let value = utils.getValue(record, column.key);
 
@@ -53,28 +60,40 @@ define(['../override', 'jquery', '../utils'], function(override, $, utils) {
                             }
 
                             let dropDownList = document.createElement('select');
+
                             dropDownList.setAttribute('id', utils.getValue(record, 0) + '-key-' + column.key);
                             dropDownList.setAttribute('class', 'pg-dropDownList');
 
                             if (options.showEmptyOption && options.showEmptyOption(column._key, column.key)) {
-                                // render empty option
+
                                 let option = document.createElement('option');
+
                                 option.setAttribute('value', undefined);
                                 option.textContent = '---';
-                                if (value === "") option.setAttribute('selected', 'true');
+
+                                if (value === '')  {
+                                    option.setAttribute('selected', 'true');
+                                }
+
                                 dropDownList.appendChild(option);
                             }
 
                             // render options
                             options.getOptionsForColumn && options.getOptionsForColumn(column._key, column.key).forEach(function(element) {
                                 let option = document.createElement('option');
+
                                 option.setAttribute('value', element.value);
                                 option.textContent = element.label;
-                                if (element.value === value) option.setAttribute('selected', 'true');
+
+                                if (element.value === value) {
+                                    option.setAttribute('selected', 'true');
+                                }
+
                                 dropDownList.appendChild(option);
                             });
 
                             cellContent.appendChild(dropDownList);
+
                             return cellContent;
                         },
                     }
